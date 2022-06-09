e6e63fd40878420e64b621b5311b03b6086af3da
Joman <German.Vlasenko@bk.ru>
2022-06-09 18:13:34 +0300

    1 part of js file

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-alert("Привет")
+//import random 
+//alert("Привет")
 
-var num = 5;
-var our = prompt("Ввод");
+var num = 0;
+//var our = prompt("Ввод");
 
 function gameGuess(num,our) {
     if (our > num) {
@@ -15,5 +16,20 @@ function gameGuess(num,our) {
 
     }
 }
+//gameGuess(num,our);
 
-gameGuess(num,our);
+function reminder () {
+    alert('А-нука отвечай, а то выкину!');
+//    window.close;
+}
+
+//setTimeout(reminder,5000);
+function valide() {
+    var name = document.getElementById('name').value;
+    var password = document.getElementById('password').value;
+    
+    var reg_name = /^[a-zа-я]+$/i;
+    var pass_name = /^[a-z0-9]{5,100}$/i
+}
+
+document.querySelector('.button').addEventListener('click', valide);
