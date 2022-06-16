bfaf0978567f2e5d45811495e41e6f473e1395d9
Joman_V <German.Vlasenko@bk.ru>
2022-06-16 14:15:02 +0300

    change button, style of button, location elements; add opportunity move between sites; save right answers of that site.

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
 'use strict';
 let domen = document.location.pathname;
-if(domen == '/index.html'){
-document.querySelector('button').addEventListener('click', () => {
+if (domen == '/index.html') {
+  localStorage.clear();
+  document.querySelector('.button').addEventListener('click', () => {
     let name = document.getElementById('name').value
-        , reg_name = /^[a-zа-я]+$/i;
+      , reg_name = /^[a-zа-я]+$/i;
     if (!reg_name.test(name)) {
-        alert('error');
-        document.getElementById('name').value = '';
-    } else {
-        localStorage.setItem('name', name);
-        location.href = "task_1.html";
+      alert('error');
+      document.getElementById('name').value = '';
     }
-});
+    else {
+      localStorage.setItem('Name', name);
+      location.href = "task_1.html";
+    }
+  });
 }
-
-else if(domen == '/task_1.html') {
-  
-    document.querySelector('div[class="choice"]').addEventListener('click', () => {
+else if (domen == '/page_result.html') {
+  document.querySelector('.button').addEventListener('click', () =>{
+    localStorage.clear();
+    location.href = 'index.html';
+  })
+}
+else {
+  for (let j = 1; j <= 5; j++) {
+    if (domen == '/task_' + String(j) + '.html') {
       let rightAnswers = 0;
-      for(let i = 1; i<=4; i++){
-        let _element = document.getElementById(String(i));
-        
-        console.log(_element.value)
+      document.querySelector('.button').addEventListener('click', () => {
+        rightAnswers = 0;
+        for (let i = 1; i <= 4; i++) {
+          let _element = document.getElementById(String(i));
+          if (_element.checked == _element.value) rightAnswers += 1;
         }
-    });
-
+        localStorage.setItem('Right Answers_' + String(j), rightAnswers);
+        location.href = j != 5 ? "task_" + String(j + 1) + ".html" : 'page_result.html';
+      });
+    }
+  }
 }
